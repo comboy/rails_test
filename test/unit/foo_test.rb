b2f3ce44b39dabf2d9127d3a91446f8804f75575
comboy <kacper.ciesla@gmail.com>
2012-03-26 13:47:42 +0200

    introduce one unit failing

--- test/unit/foo_test.rb
+++ test/unit/foo_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class FooTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
-    assert true
+    assert false
   end
 
   test "something else" do
